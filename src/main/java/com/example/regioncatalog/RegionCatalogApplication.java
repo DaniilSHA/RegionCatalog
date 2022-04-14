019ae53fc49b19401a8475000f062bffb21dc962
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-04-14 17:44:39 +0300

    Initial commit

---
+++ src/main/java/com/example/regioncatalog/RegionCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.regioncatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/regioncatalog/RegionCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regioncatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
